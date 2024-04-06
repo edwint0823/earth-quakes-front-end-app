99d408af917666a8867540ae197dcc2031783b27
Edwin Tobias Ariza Tellez <edwint0823@gmail.com>
2024-04-06 14:44:00 -0500

    project styles and libraries config

---
+++ src/layout/main.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="flex-grow p-4 bg-slate-300/50" style="min-height: 100vh">
+    <div class="shadow-blur rounded-2xl bg-white/80 p-5">
+      <router-view/>
+    </div>
+  </main>
+</template>
+<script setup>
+</script>
+<style scoped>
+</style>
